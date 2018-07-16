d1e65a182b28e641a9d47c2df6fd1b61e179213d
Jake Jenkins <jenkinsjake04@gmail.com>
2018-07-15 23:36:52 -0400

    IFrame

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,5 @@
 function start() {
   var apc = parseInt(document.getElementById("apc").value);
   var Iframe = document.getElementById("GameArea");
-  iframe.contentWindow.postMessage(apc);
+  Iframe.contentWindow.postMessage(apc);
 }
