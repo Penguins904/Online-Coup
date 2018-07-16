6f9faf3f561f065203d1b03f84c5e11080b287b9
Jake Jenkins <jenkinsjake04@gmail.com>
2018-07-15 23:44:58 -0400

    Iframe

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,5 @@
 function start() {
   var apc = parseInt(document.getElementById("apc").value);
   var Iframe = document.getElementById("GameArea");
-  Iframe.contentWindow.postMessage(apc);
+  Iframe.contentWindow.postMessage(apc, "https://penguins904.github.io/");
 }
